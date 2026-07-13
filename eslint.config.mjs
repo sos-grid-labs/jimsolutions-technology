0c43b3f4c42f6c01680326f2edf4d3bfa5fcff7c
Univastinct <centracouncil@gmail.com>
2026-07-13 01:21:28 +0100

    chore: configure Prettier, ESLint flat config integration, Tailwind v4 theme, and Font Awesome

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
